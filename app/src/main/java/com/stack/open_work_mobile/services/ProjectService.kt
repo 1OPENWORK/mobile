1a38e49f922f47013d2ebc051c2dcee656dfe2ef
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-09-30 18:15:30 -0300

    refactor: altered one for list

--- app/src/main/java/com/stack/open_work_mobile/services/ProjectService.kt
+++ app/src/main/java/com/stack/open_work_mobile/services/ProjectService.kt
@@ -1,11 +1,11 @@
 package com.stack.open_work_mobile.services
 
+import com.stack.open_work_mobile.activities.lay_my_projects.ProjectProgressCard
 import com.stack.open_work_mobile.activities.lay_my_projects.ProjectProgressCardAdapter
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ProjectService {
-
-    @GET("/projetos-aceitos/andamento/")
-    fun getAllCompleted(): Call<ProjectProgressCardAdapter>
+    @GET("/projetos-aceitos/andamento/desenvolvedor/3")
+    fun getAllProgress(): Call<List<ProjectProgressCard>>
 }

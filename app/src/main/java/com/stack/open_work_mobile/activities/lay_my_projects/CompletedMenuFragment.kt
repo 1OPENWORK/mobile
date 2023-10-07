7053ad4b66c80403c2ff4eabe1258013c6f715be
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-10-07 11:21:00 -0300

    feat: connection url completed card

--- app/src/main/java/com/stack/open_work_mobile/activities/lay_my_projects/CompletedMenuFragment.kt
+++ app/src/main/java/com/stack/open_work_mobile/activities/lay_my_projects/CompletedMenuFragment.kt
@@ -1,13 +1,20 @@
 package com.stack.open_work_mobile.activities.lay_my_projects
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.stack.open_work_mobile.R
+import com.stack.open_work_mobile.api.Rest
+import com.stack.open_work_mobile.services.ProjectService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +23,7 @@ private const val ARG_PARAM2 = "param2"
 
 
 private lateinit var adapter: ProjectProgressCardAdapter
-private lateinit var recyclerView: RecyclerView
+private lateinit var recycleView: RecyclerView
 private lateinit var projectCardCompleted: ArrayList<ProjectProgressCard>
 
 
@@ -32,18 +39,26 @@ class CompletedMenuFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        projectCardCompleted = ArrayList()
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_completed_menu, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_completed_menu, container, false)
+        recycleView = view.findViewById(R.id.recycle_view_completed)
+        recycleView.layoutManager = LinearLayoutManager(requireContext())
+        recycleView.setHasFixedSize(true)
+        adapter = ProjectProgressCardAdapter(projectCardCompleted)
+        recycleView.adapter = adapter
+        return view
     }
 
     companion object {
@@ -68,34 +83,40 @@ class CompletedMenuFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        dataInit()
-
-//        val layouManager = LinearLayoutManager(context)
-//        recyclerView = view.findViewById(R.id.recycle_view_completed)
-//        recyclerView.layoutManager = layouManager
-//        recyclerView.setHasFixedSize(true)
-//        adapter = ProjectProgressCardAdapter(projectCardCompleted)
-//        recyclerView.adapter = adapter
+        loadData()
     }
 
-//    private fun dataInit() {
-//        projectCardCompleted = arrayListOf<ProjectProgressCard>()
-//
-//        title = arrayOf(
-//            "Projeto completed 01 pdvs", "Projeto Completed 02", "Projeto 3"
-//        )
-//        subTitle = arrayOf("Company 1, 11/11/1111", "CClo, 11/11/1111", "FormOu, 11/11/1111")
-//        desc = arrayOf(
-//            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-//            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text o",
-//            "Projeto 3 asohdjasnfkaskjfnkajsfkman smknfd kajsnjkl"
-//        )
-//
-//        for (i: Int in title.indices) {
-//
-//            val project = ProjectProgressCard(title[i], subTitle[i], desc[i])
-//            projectCardCompleted.add(project)
-//        }
-//
-//    }
+    private fun loadData() {
+
+        val api = Rest.getInstance()?.create(ProjectService::class.java)
+        val list: ArrayList<ProjectProgressCard> = ArrayList()
+
+        api?.getAllCompleted()?.enqueue(object : Callback<List<ProjectProgressCard>> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(
+                call: Call<List<ProjectProgressCard>>,
+                response: Response<List<ProjectProgressCard>>
+            ) {
+                if (response.isSuccessful) {
+                    val projectList = response.body()
+
+                    projectList?.forEach { current ->
+                        current.progress = 100.0
+                        list.add(current)
+                        adapter.notifyDataSetChanged()
+                    }
+                    projectCardCompleted.addAll(list)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(requireContext(), response.message(), Toast.LENGTH_LONG).show()
+                    print("Message: ${response.message()}\n" + "Error Body: ${response.errorBody()}\n" + "Header: ${response.headers()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<ProjectProgressCard>>, t: Throwable) {
+                Toast.makeText(requireContext(), t.message, Toast.LENGTH_LONG).show()
+                print("Error Api: ${t.message}")
+            }
+        })
+    }
 }
